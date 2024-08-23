de3a4ec4f59ea3280d880aedaa63753193606612
rip <rip@friday>
2024-08-22 22:06:00 -0500

    Redoing window generation, progessing in tutorials. Adding window color

--- src/main.cpp
+++ src/main.cpp
@@ -3,34 +3,69 @@
 #include "../include/glad/glad.h"
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
 int main(int, char**){
 
-    GLFWwindow* window;
+glfwInit();
+glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    if (!glfwInit()) {
-        return -1;
-    }
 
-    window = glfwCreateWindow(640, 480, "Window!", NULL, NULL);
-    glfwMakeContextCurrent(window);
+GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 
-   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cout << "Couldn't load opengl" << std::endl;
+if (window == NULL)
+{
+    std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
-    return -1;
-   }
-
-    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
-
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-    }
-
-   glfwTerminate();
-   return 0;
+    return -1; 
 }
+glfwMakeContextCurrent(window);
+
+if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+{
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+}
+
+glViewport(0, 0, 800, 600);
+
+while(!glfwWindowShouldClose(window))
+{
+    // Input
+    processInput(window);
+
+    // Commands here
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // call events and swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+}
+
+glfwTerminate();
+return 0;
+
+
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+}
+
